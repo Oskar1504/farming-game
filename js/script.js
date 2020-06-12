bb8722b2b114044cefe9a33b36237c26b862e8c7
Oskar1504 <66569441+Oskar1504@users.noreply.github.com>
2020-06-12 15:38:02 +0200

    pflanzen setup kauf setup

--- js/script.js
+++ js/script.js
@@ -1,18 +1,5 @@
 
 function start(){
-
-	var debug =  document.getElementById("debug");
-	var debug_1 =  document.getElementById("debug_1");
-	var debug_2 =  document.getElementById("debug_2");
-	var debug_3 =  document.getElementById("debug_3");
-	var debug_4 =  document.getElementById("debug_4");
-	var debug_5 =  document.getElementById("debug_5");
-	var debug_6 =  document.getElementById("debug_6");
-	var debug_7 =  document.getElementById("debug_7");
-	var debug_8 =  document.getElementById("debug_8");
-	var debug_9 =  document.getElementById("debug_9");
-	var debug_10 =  document.getElementById("debug_10");
-	
 	
 	
 	arraytest();
@@ -26,9 +13,9 @@ function create_field(id_nummer) {
 	 "data":{
 		"preis":id_nummer*100,
 		"fruit":{
-			"preis":0,
-			"wert":0,
-			"id":"none"
+			"preis":123,
+			"wert":12,
+			"id":"dick"
 		}
 	 }
 	};
@@ -36,11 +23,13 @@ function create_field(id_nummer) {
 
 var alle_felder = document.getElementsByClassName("field");
 var selected_field = "none";
+var selected_field_array_index = 0;
 
 var clock_value = 0,
-    gold = 100;
+    gold = 1000;
 
 var test = 0;
+
 
 
 function arraytest(){
@@ -64,29 +53,95 @@ function clock(){
 
 function field_select(field){
 	
-	for(var i = 0; i< alle_felder.length; i++){
+	for(var i = 0; i < alle_felder.length; i++){
 		alle_felder[i].classList.remove("selected");
+		
 	}
-	
 	field.classList.add("selected");
+	selected_field = field.id;
+	
 	for(var i = 0; i< alle_felder.length; i++){
 		if(alle_felder[i].classList.contains("selected") == true){
-			test = i;
+			selected_field_array_index = i;
 		}
 	}
 	
-	debug.value = fields[test].id;
-	debug_1.value = fields[test].id;
-	debug_3.value = fields[test].data.preis;	
+
+	//zeigtr alle values
+	show_values();
+	
 }
 
 function kaufen(){
-	document.getElementById(selected_field).classList.add("gekauft");
+	if(gold >= fields[selected_field_array_index].data.preis){
+		gold = gold-fields[selected_field_array_index].data.preis;
+		document.getElementById(selected_field).classList.remove("locked");
+		document.getElementById(selected_field).classList.add("gekauft");
+	}
+	
 }
 
 function show_values(){
 	//zeigt alle werte 
 	document.getElementById("clock_value").value = clock_value;
 	document.getElementById("gold").value = gold;
+	document.getElementById("array_index").value = selected_field_array_index;
+	document.getElementById("preis").value = fields[selected_field_array_index].data.preis;
+	
+	fruit_2();
 }
+
+
+function swag(){
+	zeile = document.getElementById("test");
+	
+	if(zeile.classList.contains("hide") == true){
+		zeile.classList.remove("hide");
+	}else{
+		zeile.classList.add("hide");
+	}
+	
+}
+
+function swag_2(){
+	zeile = document.getElementById("test_2");
+	
+	if(zeile.classList.contains("hide") == true){
+		zeile.classList.remove("hide");
+	}else{
+		zeile.classList.add("hide");
+	}
+	
+}
+
+function change_function(){
+	document.getElementById("button").setAttribute("onclick","swag_2()");
+}
+
+
+function fruit_2(){
+	document.getElementById("fruit_id").value = salat.id;
+	document.getElementById("fruit_preis").value = salat.preis;
+	document.getElementById("fruit_wert").value = salat.wert;
+	
+	document.getElementById("ffruit_id").value = fields[selected_field_array_index].data.fruit.id;
+	document.getElementById("ffruit_preis").value = fields[selected_field_array_index].data.fruit.preis;
+	document.getElementById("ffruit_wert").value = fields[selected_field_array_index].data.fruit.wert;
+}
+
+function pflanze_salat(){
+	fields[selected_field_array_index].data.fruit.id = fruit.salat.id;
+	fields[selected_field_array_index].data.fruit.preis = fruit.salat.preis;
+	fields[selected_field_array_index].data.fruit.wert = fruit.salat.wert;
+	fruit_2();
+}
+
+function pflanze_mohre(){
+	fields[selected_field_array_index].data.fruit.id = fruit.mohre.id;
+	fields[selected_field_array_index].data.fruit.preis = fruit.mohre.preis;
+	fields[selected_field_array_index].data.fruit.wert = fruit.mohre.wert;
+	fruit_2();
+}
+
+
 
--- js/speicher.js
+++ js/speicher.js
@@ -1,2 +1,21 @@
 var fields = [];
 
+var salat ={
+	"id" : "salat",
+	"preis":10,
+	"wert":20
+};
+
+var fruit = {
+	"salat":{
+		"id":"salat",
+		"preis":20,
+		"wert":40
+	},
+	"mohre":{
+		"id":"Möhre",
+		"preis":60,
+		"wert":120
+		
+	}
+}
