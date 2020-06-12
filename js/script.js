0d5dea3432fbcc322c20ae11810ee3cf00c03e31
Oskar1504 <66569441+Oskar1504@users.noreply.github.com>
2020-06-12 20:47:51 +0200

    pflanzen,ernten

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 
 function start(){
 	
-	
 	arraytest();
 	clock();
-	
 }
 
 function create_field(id_nummer) {
   return {
 	 "id":"field_"+id_nummer,
-	 "data":{
-		"preis":id_nummer*100,
-		"fruit":{
-			"preis":123,
-			"wert":12,
-			"id":"dick"
-		}
+	 "growstart":0,
+	 "preis":id_nummer*100,
+	 "fruit":{
+		"preis":0,
+		"wert":0,
+		"id":"none",
+		"growduration":5
 	 }
+	 
 	};
 };
 
 var alle_felder = document.getElementsByClassName("field");
-var selected_field = "none";
+var selected_field = "field_0";
 var selected_field_array_index = 0;
+
+var alle_shop_button = document.getElementsByClassName("shop_button");
+var selected_shop_item = "none";
 
 var clock_value = 0,
     gold = 1000;
-
-var test = 0;
 
 
 
@@ -41,6 +41,8 @@ function arraytest(){
 
 function clock(){
 	setTimeout(function(){
+		// checks if harvestable
+		check_harvestable();
 		//zählt clock hoch
 		clock_value++;
 		//zeigtr alle values
@@ -50,6 +52,21 @@ function clock(){
 	
 	},1000);
 }
+
+function check_harvestable(){
+	for(var i = 0; i< alle_felder.length; i++){
+		
+		if( fields[i].growstart + fields[i].fruit.growduration <= clock_value){
+			
+			if(alle_felder[i].classList.contains("harvestable")==false&&fields[i].fruit.id != "none"){
+			alle_felder[i].classList.add("harvestable");
+			console.log(fields[i].id + " harvestable");
+			
+			}
+		}
+	}
+}
+	 
 
 function field_select(field){
 	
@@ -69,16 +86,66 @@ function field_select(field){
 
 	//zeigtr alle values
 	show_values();
+	show_details();
+}
+
+function shop_select_item(item){
 	
+	for(var i = 0; i < alle_shop_button.length; i++){
+		alle_shop_button[i].classList.remove("selected");
+		
+	}
+	item.classList.add("selected");
+	selected_shop_item = item.classList[1];
+	shop_table_info();
+
+}
+
+function plant(){
+	
+	if(document.getElementById(selected_field).classList.contains("locked") == false){
+		
+		//stores fruit values into select field storage
+		fields[selected_field_array_index].fruit.id = jsonPathToValue(fruit, selected_shop_item +".id");
+		fields[selected_field_array_index].fruit.preis = jsonPathToValue(fruit, selected_shop_item +".preis");
+		fields[selected_field_array_index].fruit.wert = jsonPathToValue(fruit, selected_shop_item +".wert");
+		fields[selected_field_array_index].fruit.growduration = jsonPathToValue(fruit, selected_shop_item +".growduration");
+		// removes cost from gold
+		gold = gold-fields[selected_field_array_index].fruit.preis;
+		//stores clock_value
+		fields[selected_field_array_index].growstart = clock_value;
+		
+		console.log("planted");
+	}else{
+		alert("You cant plant on locked feilds.");
+	}
+	show_details();
 }
 
 function kaufen(){
-	if(gold >= fields[selected_field_array_index].data.preis){
-		gold = gold-fields[selected_field_array_index].data.preis;
+
+	if(gold >= fields[selected_field_array_index].preis){
+		gold = gold-fields[selected_field_array_index].preis;
 		document.getElementById(selected_field).classList.remove("locked");
 		document.getElementById(selected_field).classList.add("gekauft");
 	}
 	
+}
+
+
+
+function ernten(){
+	if(document.getElementById(selected_field).classList.contains("harvestable") == true){
+		//removes harvestable grafik
+		document.getElementById(selected_field).classList.remove("harvestable");
+		//adds gold
+		gold = gold + fields[selected_field_array_index].fruit.wert;
+		//resets fields fruit values
+		fields[selected_field_array_index].fruit.id = "none";
+		fields[selected_field_array_index].fruit.preis = 0;
+		fields[selected_field_array_index].fruit.wert = 0;
+		fields[selected_field_array_index].fruit.growduration = 0;
+	}
 }
 
 function show_values(){
@@ -86,62 +153,67 @@ function show_values(){
 	document.getElementById("clock_value").value = clock_value;
 	document.getElementById("gold").value = gold;
 	document.getElementById("array_index").value = selected_field_array_index;
-	document.getElementById("preis").value = fields[selected_field_array_index].data.preis;
+	document.getElementById("preis").value = fields[selected_field_array_index].preis;
 	
-	fruit_2();
+}
+
+function show_details(){
+	if(document.getElementById("detail_checkbox").checked == true){
+		document.getElementById("detail_field_id").value = fields[selected_field_array_index].id;
+		document.getElementById("detail_field_preis").value = fields[selected_field_array_index].preis;
+		document.getElementById("detail_field_fruit").value = fields[selected_field_array_index].fruit.id;
+		document.getElementById("detail_field_growstart").value = fields[selected_field_array_index].growstart;
+		
+		document.getElementById("detail_fruit_id").value = fields[selected_field_array_index].fruit.id;
+		document.getElementById("detail_fruit_preis").value = fields[selected_field_array_index].fruit.preis;
+		document.getElementById("detail_fruit_wert").value = fields[selected_field_array_index].fruit.wert;
+		document.getElementById("detail_fruit_growduration").value = fields[selected_field_array_index].fruit.growduration;
+	}
 }
 
 
-function swag(){
-	zeile = document.getElementById("test");
-	
-	if(zeile.classList.contains("hide") == true){
-		zeile.classList.remove("hide");
+function change_opasity(iziel){
+	ziel = document.getElementById(iziel);
+	if(ziel.classList.contains("hide") == true){
+		ziel.classList.remove("hide");
 	}else{
-		zeile.classList.add("hide");
+		ziel.classList.add("hide");
 	}
 	
 }
 
-function swag_2(){
-	zeile = document.getElementById("test_2");
-	
-	if(zeile.classList.contains("hide") == true){
-		zeile.classList.remove("hide");
-	}else{
-		zeile.classList.add("hide");
-	}
-	
+
+function change_function(ziel){
+	document.getElementById(ziel).setAttribute("onclick","debug(\"swag\")");
 }
 
-function change_function(){
-	document.getElementById("button").setAttribute("onclick","swag_2()");
+function debug(output){
+	console.log(output);
 }
 
-
-function fruit_2(){
-	document.getElementById("fruit_id").value = salat.id;
-	document.getElementById("fruit_preis").value = salat.preis;
-	document.getElementById("fruit_wert").value = salat.wert;
-	
-	document.getElementById("ffruit_id").value = fields[selected_field_array_index].data.fruit.id;
-	document.getElementById("ffruit_preis").value = fields[selected_field_array_index].data.fruit.preis;
-	document.getElementById("ffruit_wert").value = fields[selected_field_array_index].data.fruit.wert;
+function shop_table_info(){
+	document.getElementById("shop_table_info").innerHTML = "Id:" + jsonPathToValue(fruit, selected_shop_item +".id") +"\n" +"Preis:"+jsonPathToValue(fruit, selected_shop_item +".preis")+"\n" +"Wert:"+jsonPathToValue(fruit, selected_shop_item +".wert")+"\n" +"Growduration:"+jsonPathToValue(fruit, selected_shop_item +".growduration");
 }
 
-function pflanze_salat(){
-	fields[selected_field_array_index].data.fruit.id = fruit.salat.id;
-	fields[selected_field_array_index].data.fruit.preis = fruit.salat.preis;
-	fields[selected_field_array_index].data.fruit.wert = fruit.salat.wert;
-	fruit_2();
-}
-
-function pflanze_mohre(){
-	fields[selected_field_array_index].data.fruit.id = fruit.mohre.id;
-	fields[selected_field_array_index].data.fruit.preis = fruit.mohre.preis;
-	fields[selected_field_array_index].data.fruit.wert = fruit.mohre.wert;
-	fruit_2();
-}
-
-
-
+function jsonPathToValue(jsonData, path) {
+	//function created by George Siggouroglou https://stackoverflow.com/questions/8790607/javascript-json-get-path-to-given-subnode
+    if (!(jsonData instanceof Object) || typeof (path) === "undefined") {
+        throw "Not valid argument:jsonData:" + jsonData + ", path:" + path;
+    }
+    path = path.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+    path = path.replace(/^\./, ''); // strip a leading dot
+    var pathArray = path.split('.');
+    for (var i = 0, n = pathArray.length; i < n; ++i) {
+        var key = pathArray[i];
+        if (key in jsonData) {
+            if (jsonData[key] !== null) {
+                jsonData = jsonData[key];
+            } else {
+                return null;
+            }
+        } else {
+            return key;
+        }
+    }
+    return jsonData;
+}  
--- js/speicher.js
+++ js/speicher.js
@@ -7,15 +7,46 @@ var salat ={
 };
 
 var fruit = {
+	"none":{
+		"id":"none",
+		"preis":0,
+		"wert":0,
+		"growduration":0
+	},
 	"salat":{
 		"id":"salat",
 		"preis":20,
-		"wert":40
+		"wert":40,
+		"growduration":10
 	},
 	"mohre":{
 		"id":"Möhre",
 		"preis":60,
-		"wert":120
-		
+		"wert":120,
+		"growduration":200
+	},
+	"erbse":{
+		"id":"Erbse",
+		"preis":60,
+		"wert":120,
+		"growduration":350
+	},
+	"aubergine":{
+		"id":"Aubergine",
+		"preis":220,
+		"wert":500,
+		"growduration":500
+	},
+	"kurbis":{
+		"id":"Kürbis",
+		"preis":650,
+		"wert":1800,
+		"growduration":2000
+	},
+	"weizen":{
+		"id":"Weizen",
+		"preis":1000,
+		"wert":2500,
+		"growduration":5000
 	}
-}
+};
